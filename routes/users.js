794954a0ad82fb3b590d2f75a2cf45b2f927ddaa
Jenny Anne Höök <93882622+JennyAnneHook@users.noreply.github.com>
2022-07-27 19:45:42 +0200

    finish

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { json } = require('express/lib/response');
 var router = express.Router();
+const {ObjectId} = require('mongodb');
 
 
 // Får användare från datorbas
@@ -9,23 +10,34 @@ router.get('/', function(req, res, next) {
   req.app.locals.db.collection('users').find().toArray()
   .then(results =>{
     console.log(results);
-    let printUsers = '<div><h2>Våra users</h2>'
+    let printUsers = '<div><h1>Våra users</h1>'
 
     for (user in results){
-      printUsers += '<div>' + results[user].username + '</div>'
+      printUsers += '<h2>' + results[user].username + '</h2>' 
+      + '<p>' + 'Prenumerationsstatus: ' +results[user].subscribe +  '</p>'
+      
     }
-    printUsers += "</div>"
+    printUsers += '</div>'
     res.send(printUsers);
   })
 });
 
+// Hittar en och skriver ut det
+
+router.post('/user', function(req ,res, next){
+  req.app.locals.db.collection('users').findOne({_id: ObjectId(req.body._id)})
+  .then(results =>{
+    res.json(results)
+  })
+});
 
 // hämtar users till frontend
 router.post('/loggin', function(req, res, next) {
 
-  req.app.locals.db.collection('users').find().toArray()
+  console.log(req.body);
+  req.app.locals.db.collection('users').findOne({username: req.body.username, password: req.body.password})
   .then(results =>{
-    console.log(results);
+    res.json(results)
   })
 });
 
@@ -39,28 +51,20 @@ router.post('/add', function(req,res){
   })
 });
 
-// router.get('/loggin', function (req, res, next){
-//   req.app.locals.db.collection('users').find().toArray()
-//   .then(results =>{
-
-//     let users = results
-    
-//     res.send(users);
-//   })
-// })
-
-// router.post("/loggin", function (req, res, next) {
-
-//   const obj = JSON.parse(JSON.stringify(req.body));
-
-//   if (obj.username == "admin" && obj.password == "admin") {
-//     console.log("OOK");
-    
-//   } else {
-//     console.log("Inte OK");
-//   }
-
-//   return;
-// });
+// Ändra gammal Posts // PUT
+router.put('/:id', function(req,res){
+  console.log(req.params.id)
+  console.log(req.body.subscribe);
+  try {
+    req.app.locals.db.collection('users').findOneAndUpdate({username: req.body.username}, {$set: {subscribe: req.body.subscribe}})
+    .then(result =>{
+      res.status(200).send(JSON.stringify('200'));
+      console.log('uppdaterar användare', result);
+    })
+  } catch (err){
+    console.log(err);
+    res.status(400).send(err)
+  }
+});
 
 module.exports = router;
